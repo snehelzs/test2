fb478cf95947eed43cb73d533bd5432e9dfb8f5e
snehelzs <snehelsinha@gmail.com>
2024-09-13 07:54:56 +0000

    Update tabSlice.js

--- tabSlice.js
+++ tabSlice.js
@@ -2,7 +2,8 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   currentTab: 0, 
-  healthCheckerResult: null
+  healthCheckerResult: null,
+  valueCheckerResult: null
 };
 
 const tabSlice = createSlice({
@@ -13,11 +14,16 @@ const tabSlice = createSlice({
       state.currentTab = action.payload;
     },
     setHealthCheckerResult: (state, action) => {
+      console.log(action.payload,"reduxx")
       state.healthCheckerResult = action.payload;
+    },
+    setValueCheckerResult: (state, action) => {
+      console.log(action.payload,"reduxx")
+      state.valueCheckerResult = action.payload;
     },
   },
 });
 
-export const { setCurrentTab, setHealthCheckerResult } = tabSlice.actions;
+export const { setCurrentTab, setHealthCheckerResult, setValueCheckerResult } = tabSlice.actions;
 
 export default tabSlice.reducer;
